34fba0df86bbeed28268e15ba5fb6d02dd86881a
lynettenyambura <linetnyambura45@gmail.com>
2024-03-03 19:10:26 +0300

    values&variables

---
+++ assignments.js
@@ -0,0 +1,6 @@
+let country = 'Kenya';
+let continent = 'Africa';
+let population = 53.01 ;
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ script.js
@@ -0,0 +1,3 @@
+let js = "amazing";
+
+console.log(2 + 3 + 4 - 5);//
